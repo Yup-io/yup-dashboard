d6ddbf95041351285d66e829982d7e0e50e026f4
daniiba <danielibanezbecker@aol.com>
2020-12-02 13:34:36 +0100

    iframe styles

--- src/app/pages/YupPage.js
+++ src/app/pages/YupPage.js
@@ -28,14 +28,14 @@ suhbeader.setTitle("📊 Yup.Info");
     src="https://app.uniswap.org/#/swap?use=v1?outputCurrency=0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359"
     height="660px"
     width="100%"
-    style="
-      border: 0;
-      margin: 0 auto;
-      display: block;
-      border-radius: 10px;
-      max-width: 600px;
-      min-width: 300px;
-    "
+    style={{ 
+      border: 0,
+      margin: 0+ " auto",
+      display: "block",
+      borderRadius: 10+"px",
+      maxWidth: 600+"px",
+      minWidth: 300+"px",
+    }}
     id="myId"
   />
   </div>
