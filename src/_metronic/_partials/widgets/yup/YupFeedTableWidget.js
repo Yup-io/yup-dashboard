a302ae5a25ed9eae5484dcd6c229453586bc0484
ncucciniello <ncucciniello@gmail.com>
2021-02-18 14:42:02 -0500

    add like/dislike filter & dynamic pages, edit table css

--- src/_metronic/_partials/widgets/yup/YupFeedTableWidget.js
+++ src/_metronic/_partials/widgets/yup/YupFeedTableWidget.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-script-url,jsx-a11y/anchor-is-valid,jsx-a11y/img-redundant-alt */
 import React, { Component } from "react";
-import {Pagination} from "react-bootstrap";
+import {Pagination, ButtonGroup, ToggleButton} from "react-bootstrap";
 import { Loader } from './loader'
 import './custom.css';
 import Skeleton from '@material-ui/lab/Skeleton';
@@ -18,7 +18,9 @@ class YupFeedTableWidget extends Component {
       error: null,
       isLoaded: false,
       items: { 1:null,2:null,3:null,4:null,5:null},
-      page: 1
+      page: 1,
+      radioValue: '1',
+      numOfVotes: 0
     };
     this.updateData = this.updateData.bind(this);
     this.getCreateVoteV3 = this.getCreateVoteV3.bind(this);
@@ -28,26 +30,31 @@ class YupFeedTableWidget extends Component {
   componentDidMount() {
     this.updateData(1, 50)
   // setInterval(this.updateData.bind(null, 1, 50), 60000);
-
   }
-  async updateData(page, limit) {
 
+  async updateData(page, limit) {
     let [createVoteV3,postVoteV2]= await Promise.all([this.getCreateVoteV3(0,limit), this.getPostVoteV2(0,limit)]);
     let allVotes = createVoteV3.actions.concat(postVoteV2.actions)
     allVotes.sort(function(a, b){return new Date(b.timestamp) - new Date(a.timestamp)});
     allVotes.forEach(item => {
       item.timestamp = this.convertUTCDateToLocalDate(new Date(item.timestamp))
     })
+    if (this.state.radioValue == '2') {
+      allVotes = allVotes.filter(vote => vote.act.data.like == true)
+    }
+    else if (this.state.radioValue == '3') {
+      allVotes = allVotes.filter(vote => vote.act.data.like == false)
+    }
+    this.setState({numOfVotes: allVotes.length})
     let items = allVotes.slice(10*(page-1),(10*page))
-   this.getPostData(page, items)
-
+    this.getPostData(page, items)
   }
-
 
   convertUTCDateToLocalDate(date) {
-      var newDate = new Date(date.getTime() - date.getTimezoneOffset()*60*1000);
-      return newDate;
+    var newDate = new Date(date.getTime() - date.getTimezoneOffset()*60*1000);
+    return newDate;
   }
+
   async getCreateVoteV3(skip, limit){
     return new Promise( (resolve, reject) =>{ fetch("https://eos.hyperion.eosrio.io/v2/history/get_actions?account=yupyupyupyup&filter=*%3Acreatevotev3&skip="+skip+"&limit="+limit+"&sort=desc")
     .then(res => res.json())
@@ -58,20 +65,22 @@ class YupFeedTableWidget extends Component {
       (error) => {
         reject(error)
       })
-      })
-    }
+    })
+  }
+
   async getPostVoteV2(skip, limit){
-      return new Promise( (resolve, reject) =>{ fetch("https://eos.hyperion.eosrio.io/v2/history/get_actions?account=yupyupyupyup&filter=*%3Apostvotev2&skip="+skip+"&limit="+limit+"&sort=desc")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          resolve(result)
-        },
-        (error) => {
-          reject(error)
-        })
-        })
-    }
+    return new Promise( (resolve, reject) =>{ fetch("https://eos.hyperion.eosrio.io/v2/history/get_actions?account=yupyupyupyup&filter=*%3Apostvotev2&skip="+skip+"&limit="+limit+"&sort=desc")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        resolve(result)
+      },
+      (error) => {
+        reject(error)
+      })
+    })
+  }
+
   async getPostData(page,items) {
     let fullData = [];
     var itemsProcessed = 0;
@@ -79,70 +88,66 @@ class YupFeedTableWidget extends Component {
     if(!this.state.items[page] || items[0].global_sequence!=this.state.items[page][0].vote.global_sequence){
       this.setState({
         isLoaded: false
-      });
+      })
 
       await items.forEach(async vote => {
-      if(vote.act.data.postid){
-       await fetch("https://api.yup.io/posts/post/"+vote.act.data.postid, {
-        method: 'GET'
-      })
-        .then(res => {
-          try {
-          return res.json()
+        if(vote.act.data.postid){
+          await fetch("https://api.yup.io/posts/post/"+vote.act.data.postid, {
+            method: 'GET'
+          })
+          .then(res => {
+            try {
+              return res.json()
+            }
+            catch(e){
+              console.log(e)
+              this.setState({
+                isLoaded: false,
+                error:e
+              });
+            }
+          }).then( (result) => {
+            if(result){
+              fullData.push({
+                post:{
+                  caption:result.caption,
+                  tag: result.tag
+                },
+                vote: vote
+              })
+            }
+          })
+        } else {
+          fullData.push({
+            vote: vote,
+            post:{
+              caption:vote.act.data.caption,
+              tag: vote.act.data.tag
+            },
+            vote: vote
+          })
         }
-        catch(e){
-          console.log(e)
+        
+        itemsProcessed++;
+        if(itemsProcessed === items.length) {
+          fullData.sort(function(a, b){return new Date(b.vote.timestamp).getTime() - new Date(a.vote.timestamp).getTime()});
+          let newItems = this.state.items
+          newItems[page] = fullData
           this.setState({
-            isLoaded: false,
-            error:e
+            isLoaded: true,
+            items: newItems,
+            page: page
           });
         }
       })
-        .then( (result) => {
-          if(result){
-          fullData.push({
-            post:{
-            caption:result.caption,
-            tag: result.tag
-          },
-          vote: vote
-        })
-        }
-        })
-      }
-      else {
-        fullData.push({
-          vote: vote,
-          post:{
-          caption:vote.act.data.caption,
-          tag: vote.act.data.tag
-        },
-        vote: vote
-      })
-      }
-      itemsProcessed++;
-      if(itemsProcessed === items.length) {
-        fullData.sort(function(a, b){return new Date(b.vote.timestamp).getTime() - new Date(a.vote.timestamp).getTime()});
-        let newItems = this.state.items
-        newItems[page] = fullData
-        this.setState({
-          isLoaded: true,
-          items: newItems,
-          page: page
-        });
-      }
-      })
-    }
-
-    else{
+    } else{
       this.setState({
         isLoaded: true,
         page: page
       });
-
     }
-
   }
+
   nextPage(){
     if(this.state.page<5){
       let page = this.state.page+1
@@ -152,17 +157,19 @@ class YupFeedTableWidget extends Component {
       });
       this.updateData(page,50)
     }
- }
- lastPage(){
-  if(this.state.page>1){
-    let page = this.state.page-1
-    this.setState({
-      isLoaded: false,
-      page: page
-    });
-    this.updateData(page,50)
   }
-}
+
+  lastPage(){
+    if(this.state.page>1){
+      let page = this.state.page-1
+      this.setState({
+        isLoaded: false,
+        page: page
+      });
+      this.updateData(page,50)
+    }
+  }
+
   createRating(n){
     let rating = n.rating
     let like = n.like
@@ -188,27 +195,42 @@ class YupFeedTableWidget extends Component {
       "3": "#F0C800",
       "4": "#7FBA1B",
       "5": "#00EAB7",
-    };
+    }
     for(let i =0; i < rating; i++){
-        elements.push( <svg key={i} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="20px" height="20px">
-        <g fill={colorMap[rating]}   > <circle cx="50%" cy="50%" r="25" /></g>
+      elements.push( <svg key={i} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="20px" height="20px">
+      <g fill={colorMap[rating]}   > <circle cx="50%" cy="50%" r="25" /></g>
       </svg>);
     }
     return elements;
-}
-  setRowColor(n) {
-    return n ? 'bg-like' : 'bg-dislike' 
+  }
+  
+  setRadioValue(e) {
+    this.setState({ radioValue: e })
   }
 
-  createPagination(){
-
+  createPagination(n) {
+    let numOfPages = Math.ceil(n / 10 )
+    return Array.from(Array(numOfPages), (_, i) => <Pagination.Item key={i} active={this.state.page===i+1}  onClick={() =>this.updateData(i+1,50)}> {i+1} </Pagination.Item>)
   }
+
+  getURL(url) {
+    const link = new URL(url)
+    return link.hostname
+  }
+
   render() {
     const { error, isLoaded, items } = this.state;
+
+    const radios = [
+      { name: 'All', value: '1' },
+      { name: 'Likes', value: '2' },
+      { name: 'Dislikes', value: '3' },
+    ]
+
     if (error) {
       return <div>Error: {error.message}</div>;
     }
-     else {
+    else {
       return (
         <div className={`card card-custom card-stretch gutter-b`}>
           {/* Head */}
@@ -216,13 +238,31 @@ class YupFeedTableWidget extends Component {
             <h2 className="card-title align-items-start flex-column">
               <span className="card-label font-weight-bolder text-dark">History</span>
             </h2>
+            <ButtonGroup toggle>
+              {radios.map((radio, idx) => (
+                <ToggleButton
+                  key={idx}
+                  type="radio"
+                  variant="outline-primary"
+                  name="radio-filter"
+                  value={radio.value}
+                  checked={this.state.radioValue === radio.value}
+                  onChange={(e) => {
+                    this.setRadioValue(e.currentTarget.value) 
+                    this.updateData(1,50)}
+                  }
+                >
+                  {radio.name}
+                </ToggleButton>
+              ))}
+            </ButtonGroup>
           </div>
           {/* Body */}
           <div className="card-body pt-0 pb-3">
             <div className="tab-content">
 
               <div className="table-responsive">
-                <table className="table table-head-custom  table-borderless table-vertical-center">
+                <table className="table table-head-custom table-borderless table-vertical-center table-spacing">
                   <thead>
                     <tr className="text-left">
                       <th className="text-left">Time</th>
@@ -236,7 +276,7 @@ class YupFeedTableWidget extends Component {
                   <tbody>
 
                     {items[this.state.page].map(item => (
-                      <tr key={item.vote.global_sequence} className={this.setRowColor(item.vote.act.data.like)}>
+                      <tr key={item.vote.global_sequence} className={item.vote.act.data.like ? 'like' : 'dislike'}>
                         <td>
                           <span className="text-dark-75 d-block font-size-lg">
                            {new Date(item.vote.timestamp).toLocaleDateString()} {new Date(item.vote.timestamp).toLocaleTimeString()}
@@ -249,8 +289,7 @@ class YupFeedTableWidget extends Component {
                         </td>
                         <td>
                           <a href={item.post.caption} className="text-primary d-block font-size-lg">
-                            {item.post.caption.substring(0, 30)}
-                            {item.post.caption.length>29 && '...'}
+                            {this.getURL(item.post.caption)}
                           </a>
                         </td>
                         <td>
@@ -324,13 +363,9 @@ class YupFeedTableWidget extends Component {
             </div>
           <div className="separator separator-dashed my-7"></div>
               <Pagination className="float-right" size="lg">
-                <Pagination.Prev onClick={() =>this.lastPage() } />
-                <Pagination.Item active={this.state.page==1}  onClick={() =>this.updateData(1,50)}>{1}</Pagination.Item>
-                <Pagination.Item active={this.state.page==2} onClick={() =>this.updateData(2,50)}>{2}</Pagination.Item>
-                <Pagination.Item active={this.state.page==3} onClick={() =>this.updateData(3,50)}>{3}</Pagination.Item>
-                <Pagination.Item active={this.state.page==4}  onClick={() =>this.updateData(4,50)}>{4}</Pagination.Item>
-                <Pagination.Item active={this.state.page==5}  onClick={() =>this.updateData(5,50)}>{5}</Pagination.Item>
-                <Pagination.Next onClick={() => this.nextPage()}/>
+                {this.state.numOfVotes > 10 && <Pagination.Prev onClick={() =>this.lastPage()} />}
+                  {this.createPagination(this.state.numOfVotes)}
+                {this.state.numOfVotes > 10 && <Pagination.Next onClick={() => this.nextPage()} />}
               </Pagination>
           </div>
         </div>
